d79ac172e1eb98df5b10878a185e5fcf7ccaa54f
ryanuhl99 <ryanuhl99@gmail.com>
2024-07-01 02:50:21 -0700

    im gay

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTAPIProject.Controllers
+{   
+    [Route("api/VillaApi")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVilla()
+        {
+            return new List<Villa> {
+                new Villa { Id=1, Name="Pool View" },
+                new Villa { Id=2, Name="Ocean View" }
+            }
+        }
+    }
+}
---
+++ Models/Villa.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace RESTAPIProject.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
